fcd340be2c9cc21a3281045daa4811ba5005fc3b
Pawan Kumar <pawan221b@gmail.com>
2019-04-07 20:45:35 +0530

    gradle change

--- android/app/src/main/java/com/example/trillapp/MainActivity.java
+++ android/app/src/main/java/com/example/trillapp/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends FlutterActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
-    // getWindow().addFlags(LayoutParams.FLAG_SECURE);
 
   }
 }
